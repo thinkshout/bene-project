02e3ee19d625e121d7f18d45d6a03b0ec3258179
Tauno Hogue <tauno@thinkshout.com>
2017-06-01 21:13:16 -0700

    Initial command to update profile for development

--- RoboFile.php
+++ RoboFile.php
@@ -10,4 +10,21 @@ class RoboFile extends \ThinkShout\RoboDrupal\Tasks
 {
 
 
+  public function devUpdate() {
+
+    $this->taskComposerUpdate()
+      ->option('with-dependencies')
+      ->arg('drupal/bene')
+      ->run();
+
+    // Run the installation.
+    $result = $this->taskExec('drush cim --partial')
+      ->run();
+
+    if ($result->wasSuccessful()) {
+      $this->say('Install complete');
+    }
+
+    return $result;
+  }
 }
