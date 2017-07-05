8525bbdc851855c7c944316da6f5923854a4fd94
Tauno Hogue <tauno@thinkshout.com>
2017-07-05 13:10:07 -0700

    Add robo helper to cleanup/re-install migrations

--- RoboFile.php
+++ RoboFile.php
@@ -29,4 +29,19 @@ class RoboFile extends \ThinkShout\RoboDrupal\Tasks
 
     return $result;
   }
+
+  /**
+   * Cleanup migrations.
+   *
+   * @option string migrations
+   *   Optional list of migrations to reset, separated by commmas.
+   */
+  public function migrateCleanup($opts = ['migrations' => '']) {
+    $migrations = explode(',', $opts['migrations']);
+    $project_properties = $this->getProjectProperties();
+    foreach ($migrations as $migration) {
+      $this->taskExec('drush mrs ' . $migration)->dir($project_properties['web_root'])->run();
+    }
+    $this->taskExec('drush mr --all && drush pmu bene_migrate_google_sheets -y && drush en bene_migrate_google_sheets -y && drush ms')->dir($project_properties['web_root'])->run();
+  }
 }
